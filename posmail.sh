4283c2fe4d1e49480a8857af108bccf3bcbc3940
Tyler <wtdewilligen@gmail.com>
2015-02-03 12:47:30 -0500

    Edit for PHP5 location

--- posmail.sh
+++ posmail.sh
@@ -2,4 +2,4 @@
 #cd ~/your_path_to_tracker
 #cd /home/madhat/sd/pos/www
 #/usr/local/bin/php mail.php
-/usr/local/php5/bin/php-cgi /home/username/public_html/pos/cron_mail.php
+/usr/bin/php-cgi cron_mail.php
