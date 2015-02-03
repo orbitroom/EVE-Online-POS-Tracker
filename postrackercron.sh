21835769575771de87c13d30315742a8f897f96e
Tyler <wtdewilligen@gmail.com>
2015-02-03 12:48:18 -0500

    Edit for PHP5 Location

--- postrackercron.sh
+++ postrackercron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #cd ~/your_path_to_tracker
-cd /var/www/EVE-Online-POS-Tracker
+#cd /var/www/EVE-Online-POS-Tracker
 #/usr/local/bin/php mail.php
-/usr/local/bin/php5 cron_updateallianceinfo.php
-/usr/local/bin/php5 cron_updatesov.php
+/usr/bin/php5 cron_updateallianceinfo.php
+/usr/bin/php5 cron_updatesov.php
 
